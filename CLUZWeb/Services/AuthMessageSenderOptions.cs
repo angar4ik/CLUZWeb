f319096f1de3894faaebba94c416cf865974197e
angar4ik <4angar@gmail.com>
2020-09-07 11:06:25 -0400

    Update AuthMessageSenderOptions.cs

--- CLUZWeb/Services/AuthMessageSenderOptions.cs
+++ CLUZWeb/Services/AuthMessageSenderOptions.cs
@@ -8,6 +8,6 @@ namespace CLUZWeb.Services
     public class AuthMessageSenderOptions
     {
         public string SendGridUser { get; set; } = "Do Not Reply";
-        public string SendGridKey { get; set; } = "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";
+        public string SendGridKey { get; set; } = "";
     }
 }
